3aa06f7cff01bc1952d162e9dea9447d84986037
Yu@macbook-pro <jvyu59jvyu@icloud.com>
2020-10-12 17:45:20 +0800

    Add server side

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 
+import renderer from './helper/renderer';
+
 const PORT = process.env.PORT || 3006;
 const app = express();
 
-app.use(express.static('./build'));
+/* Not load index.html */
+app.use(express.static('./build', { index: false }));
+
+app.get('*', (req, res) => {
+  return res.send(renderer(req));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
